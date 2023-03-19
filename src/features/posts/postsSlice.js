f24479d49f971484dbfdecf0b30ce21857927fe5
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-03-19 17:10:03 +0000

    Add a new post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -3,13 +3,20 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = [
     { id: '1', title: 'Do not eat lemon meringue', content: 'Lemon meringue not good for dress' },
     { id: '2', title: 'Remember when slim', content: 'You could get this back again' },
+    { id: '3', title: 'Never say this is my last nom of the day', content: 'Just put off nomming as long as possible' },
 ];
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducers: {}
+    reducers: {
+        postAdded(state, action) {
+            state.push(action.payload);
+        }
+    }
 });
+
+export const { postAdded } = postsSlice.actions;
 
 export const selectAllPosts = state => state.posts;
 

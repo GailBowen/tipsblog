a9aab3c4f1ec1235b79ad1d98e861cb746b2f379
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-04-08 16:04:07 +0100

    Set time ago

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,19 +1,25 @@
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postsSlice';
-import { selectAllUsers } from '../users/usersSlice';
+
+import PostAuthor  from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 import React from 'react'
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
-    const users = useSelector(selectAllUsers)
    
     console.log('PostsList -> posts', posts);
     const renderedPosts = posts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
-            <span>{users[post.userId].name}</span>
             <p className="post-content">{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+            </p>
+            <p>
+                <TimeAgo timestamp={post.date} />
+            </p>
         </article>
     ))
 
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,20 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
+import { sub } from 'date-fns';
+
 const initialState = [
-    { id: '1', title: 'Do not eat lemon meringue', content: 'Lemon meringue not good for dress', userId: '0' },
-    { id: '2', title: 'Remember when slim', content: 'You could get this back again', userId: '1' },
-    { id: '3', title: 'Never say this is my last nom of the day', content: 'Just put off nomming as long as possible', userId: '2' },
+    { 
+        id: '1',
+        title:  'Do not eat lemon meringue', 
+        content: 'Lemon meringue not good for dress', 
+        userId: '0',
+        date: sub(new Date(), { minutes: 10 }).toISOString()},
+    { 
+        id: '2', 
+        title: 'Never say this is my last nom of the day', 
+        content: 'Just put off nomming as long as possible', 
+        userId: '2',
+        date: sub(new Date(), { minutes: 5}).toISOString() },
 ];
 
 const postsSlice = createSlice({
@@ -20,6 +31,7 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
+                        date: new Date().toISOString(),
                         userId
                     }
                 }
